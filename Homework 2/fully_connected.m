963a750b9e77f0cbe3a592a4100798fb4c216deb
Anna Carlsson <anna.r.carlsson@telia.com>
2019-10-06 14:12:38 +0200

    Delete fully_connected.m

--- Homework 2/fully_connected.m
+++
@@ -1,8 +0,0 @@
-%% Homework 3, Fully connected network
-% Author: Anna Carlsson
-% Last updated: 2019-10-06
-
-%% Code
-
-% Load dataset
-[xTrain, tTrain, xValid, tValid, xTest, tTest] = LoadCIFAR(1);
