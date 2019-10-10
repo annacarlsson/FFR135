72876ea239f743f98cddd07c41ba97349e7a5976
Anna Carlsson <anna.r.carlsson@telia.com>
2019-10-10 10:08:35 +0200

    Update convolutional_networks.m

--- Homework 3/convolutional_networks.m
+++ Homework 3/convolutional_networks.m
@@ -3,3 +3,105 @@
 % Last updated: 2019-10-09
 
 %% Network 1
+clc, clear all
+
+% Load data
+[xTrain, tTrain, xValid, tValid, xTest, tTest] = LoadCIFAR(4);
+
+% Settings
+options = trainingOptions('sgdm', ...
+    'InitialLearnRate',0.001, ...
+    'MaxEpochs',120, ...
+    'MiniBatchSize',8192, ...
+    'Shuffle', 'every-epoch', ...
+    'ValidationData',{xValid,tValid}, ...
+    'ValidationFrequency',30, ...
+    'ValidationPatience',3, ...
+    'Plots','training-progress')
+
+% Define layers
+layers = [
+    imageInputLayer([32 32 3])
+    
+    convolution2dLayer([5 5], 20, 'Padding', [1 1 1 1])
+    reluLayer
+    
+    maxPooling2dLayer([2 2], 'Stride', [2 2])
+    
+    fullyConnectedLayer(50)
+    reluLayer
+    
+    fullyConnectedLayer(10)
+    softmaxLayer
+    classificationLayer];
+
+% Train network
+net = trainNetwork(xTrain, tTrain, layers, options);
+
+% Compute scores
+[pred_train, scores_train] = classify(net, xTrain);
+[pred_valid, scores_valid] = classify(net, xValid);
+[pred_test, scores_test] = classify(net, xTest);
+
+C_train = classification_error(tTrain, pred_train);
+C_valid = classification_error(tValid, pred_valid);
+C_test = classification_error(tTest, pred_test);
+
+%% Network 2
+clc, clear all
+
+% Load data
+[xTrain, tTrain, xValid, tValid, xTest, tTest] = LoadCIFAR(4);
+
+% Settings
+options = trainingOptions('sgdm', ...
+    'InitialLearnRate',0.001, ...
+    'MaxEpochs',120, ...
+    'MiniBatchSize',8192, ...
+    'Shuffle', 'every-epoch', ...
+    'ValidationData',{xValid,tValid}, ...
+    'ValidationFrequency',30, ...
+    'ValidationPatience',3, ...
+    'Plots','training-progress')
+
+% Define layers
+layers = [
+    imageInputLayer([32 32 3])
+    
+    convolution2dLayer([3 3], 20, 'Padding', [1 1 1 1])
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer([2 2], 'Stride', [2 2])
+    
+    convolution2dLayer([3 3], 30, 'Padding', [1 1 1 1])
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer([2 2], 'Stride', [2 2])
+    
+    convolution2dLayer([3 3], 50, 'Padding', [1 1 1 1])
+    batchNormalizationLayer
+    reluLayer
+    
+    fullyConnectedLayer(10)
+    softmaxLayer
+    classificationLayer];
+
+% Train network
+net = trainNetwork(xTrain, tTrain, layers, options);
+
+% Compute scores
+[pred_train, scores_train] = classify(net, xTrain);
+[pred_valid, scores_valid] = classify(net, xValid);
+[pred_test, scores_test] = classify(net, xTest);
+
+C_train = classification_error(tTrain, pred_train);
+C_valid = classification_error(tValid, pred_valid);
+C_test = classification_error(tTest, pred_test);
+
+%% Definition of functions
+function C = classification_error(target, outputs)
+length_valset = size(target,1);    
+C = 1 / length_valset * sum(outputs ~= target);
+end
