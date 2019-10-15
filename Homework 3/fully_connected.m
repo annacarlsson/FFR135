bc3577ae690984cd00a65b3a662300f714532c9a
Anna Carlsson <anna.r.carlsson@telia.com>
2019-10-15 21:12:17 +0200

    Update fully_connected.m

--- Homework 3/fully_connected.m
+++ Homework 3/fully_connected.m
@@ -1,6 +1,6 @@
 %% Homework 3, Fully connected network
 % Author: Anna Carlsson
-% Last updated: 2019-10-08
+% Last updated: 2019-10-15
 
 %% Code
 clc, clear all
@@ -14,8 +14,8 @@ xTest = xTest - mean;
 
 % Settings
 learning_rate = 0.1;
-M1 = 0;
-M2 = 0;
+M1 = 50;
+M2 = 50;
 max_epochs = 20;
 batch_size = 100;
 
@@ -23,6 +23,7 @@ batch_size = 100;
 input_size = 3072;
 output_size = 10;
 n = size(xTrain,2);
+m = size(xValid,2);
 [weights, thresholds] = initialize([M1, M2], input_size, output_size);
 
 % Create cells to save the weights and thresholds (to reuse later)
@@ -31,11 +32,46 @@ thresholds_all = cell(1, max_epochs);
 
 % Train network
 epoch = 1;
-val_errors = zeros(1, max_epochs);
-train_errors = zeros(1, max_epochs);
+val_errors = zeros(1, max_epochs + 1);
+train_errors = zeros(1, max_epochs + 1);
+
+% Compute training and validation error before any training (for plot)
+n_layer = length(weights);
+state_val_init = cell(n_layer + 1, 1);
+
+outputs_val_init = zeros(10, m);
+for j = 1 : m
+    x_val_init = xValid(:,j);
+    V_temp = x_val_init;
+    
+    % Forward propagation
+    for k = 1 : n_layer
+        V_temp = sigmoid(b(cell2mat(weights(k)), V_temp, cell2mat(thresholds(k))));
+    end
+    outputs_val_init(:,j) = V_temp;
+end
+
+% Compute validation error
+val_errors(1) = classification_error(tValid, outputs_val_init);
+
+% For each pattern in training set
+outputs_train_init = zeros(10, n);
+for j = 1 : n
+    x_train_init = xTrain(:,j);
+    V_temp = x_train_init;
+    
+    % Forward propagation
+    for k = 1 : n_layer
+        V_temp = sigmoid(b(cell2mat(weights(k)), V_temp, cell2mat(thresholds(k))));
+    end
+    outputs_train_init(:,j) = V_temp;
+end
+
+% Compute validation error
+train_errors(1) = classification_error(tTrain, outputs_train_init);
 
 disp(['----- TRAINING NETWORK -----'])
-    
+
 while (epoch <= max_epochs)
     
     % Shuffle observations in training set
@@ -50,8 +86,6 @@ while (epoch <= max_epochs)
         
         x = xTrain(:, ((i-1) * batch_size + 1) : batch_size * i);
         t = tTrain(:, ((i-1) * batch_size + 1) : batch_size * i);
-        
-        n_layer = length(weights);
         
         % For each batch, reset gradients
         [dW, dTheta] = setup_gradient_arrays([M1, M2], input_size, output_size);
@@ -92,14 +126,13 @@ while (epoch <= max_epochs)
     
     % Compute error for validation set and training set
     state_val = cell(n_layer + 1, 1);
-    m = size(xValid,2);
     
     % For each pattern in validation set
     outputs_val = zeros(10, m);
     for j = 1 : m
         x_val = xValid(:,j);
         V_temp = x_val;
-            
+        
         % Forward propagation
         for k = 1 : n_layer
             V_temp = sigmoid(b(cell2mat(weights(k)), V_temp, cell2mat(thresholds(k))));
@@ -109,14 +142,14 @@ while (epoch <= max_epochs)
     
     % Compute validation error
     temp_valerror = classification_error(tValid, outputs_val);
-    val_errors(epoch) = temp_valerror;
+    val_errors(epoch + 1) = temp_valerror;
     
     % For each pattern in training set
     outputs_train = zeros(10, n);
     for j = 1 : n
         x_train = xTrain(:,j);
         V_temp = x_train;
-            
+        
         % Forward propagation
         for k = 1 : n_layer
             V_temp = sigmoid(b(cell2mat(weights(k)), V_temp, cell2mat(thresholds(k))));
@@ -126,8 +159,8 @@ while (epoch <= max_epochs)
     
     % Compute validation error
     temp_trainerror = classification_error(tTrain, outputs_train);
-    train_errors(epoch) = temp_trainerror;
-
+    train_errors(epoch + 1) = temp_trainerror;
+    
     disp(['Epoch: ', num2str(epoch), '  Train error: ', num2str(temp_trainerror), ' Val. error: ', num2str(temp_valerror)])
     
     % Save weights and thresholds for current epoch
@@ -135,14 +168,14 @@ while (epoch <= max_epochs)
     thresholds_all{epoch} = thresholds;
     
     epoch = epoch + 1;
-
+    
 end
 
 %% Test network on test set
 % Select the best model
 [val, mod_index] = min(val_errors);
-weights = weights_all(mod_index);
-thresholds = thresholds_all(mod_index);
+weights = weights_all{mod_index};
+thresholds = thresholds_all{mod_index};
 
 % Use this model to compute test error
 outputs_test = zeros(10, size(xTest,2));
@@ -154,11 +187,43 @@ for j = 1 : size(xTest,2)
     for k = 1 : n_layer
         V_temp = sigmoid(b(cell2mat(weights(k)), V_temp, cell2mat(thresholds(k))));
     end
-    outputs_train(:,j) = V_temp;
+    outputs_test(:,j) = V_temp;
 end
 
-% Compute validation error
+% Compute test error 
 test_error = classification_error(tTest, outputs_test);
+
+%% Plot validation and train errors
+train_error_1 = [0.8994,0.6256,0.6159,0.6081,0.6102,0.5982,0.5931,0.6027,0.5992,0.5909,0.5888,0.5897,0.5977,0.6031,0.5783 0.5876,0.5794,0.5789,0.5776,0.5894,0.5867];
+val_error_1 = [0.9020,0.6432,0.6429,0.6307,0.6409,0.6320,0.6316,0.6373,0.6401,0.6361,0.6309,0.6274,0.6463,0.6466,0.6177,0.6356,0.6233,0.6252,0.6247,0.6302,0.6411];
+
+train_error_2 = [0.9001,0.6053,0.6085,0.5964,0.5891,0.5949,0.5704,0.5641,0.5609,0.5632,0.5509,0.5567,0.5475,0.5562,0.5469,0.5488,0.5471,0.5434,0.5320,0.5337,0.5370];
+val_error_2 = [0.8997,0.6229,0.6316,0.6219,0.6138,0.6248,0.6033,0.6013,0.5915,0.6003,0.5996,0.5977,0.6018,0.5998,0.6003,0.6049,0.6030,0.5984,0.5972,0.5958,0.6006];
+
+train_error_3 = [0.8954,0.8477,0.7462,0.6880,0.5924,0.5650,0.5482,0.5050,0.4888,0.4908,0.4660,0.4516,0.4478,0.4416,0.4327,0.4367,0.4228,0.4163,0.4128,0.4061,0.4073];
+val_error_3 = [0.8974,0.8496,0.7572,0.7115,0.6233,0.6072,0.5951,0.5599,0.5552,0.5531,0.5432,0.5338,0.5482,0.5407,0.5482,0.5405,0.5411,0.5429,0.5425,0.5399,0.5453];
+
+train_error_4 = [0.9006,0.8995,0.6654,0.5926,0.5609,0.5486,0.5215,0.5177,0.4982,0.4929,0.4876,0.4750,0.4721,0.4549,0.4429,0.4371,0.4317,0.4245,0.4299,0.4146,0.4175];
+val_error_4 = [0.8975,0.9020,0.6735,0.6062,0.5900,0.5862,0.5667,0.5672,0.5540,0.5514,0.5581,0.5489,0.5522,0.5481,0.5438,0.5386,0.5431,0.5401,0.5491,0.5420,0.5469];
+
+epochs = linspace(0,20,21);
+plot(epochs, train_error_1,'LineWidth',1.5,'Color',[0, 0.4470, 0.7410]); hold on
+plot(epochs, val_error_1,'--','LineWidth',1.5, 'Color',[0, 0.4470, 0.7410]);
+plot(epochs, train_error_2,'LineWidth',1.5,'Color',[0.4660, 0.6740, 0.1880]); 
+plot(epochs, val_error_2,'--','LineWidth',1.5, 'Color',[0.4660, 0.6740, 0.1880]);
+plot(epochs, train_error_3,'LineWidth',1.5,'Color',[0.6350, 0.0780, 0.1840]); 
+plot(epochs, val_error_3,'--','LineWidth',1.5, 'Color',[0.6350, 0.0780, 0.1840]);
+plot(epochs, train_error_4,'LineWidth',1.5,'Color',[0.9290, 0.6940, 0.1250]); 
+plot(epochs, val_error_4,'--','LineWidth',1.5, 'Color',[0.9290, 0.6940, 0.1250]);
+
+ax = gca;
+ax.FontSize = 11; 
+
+legend('Net 1 train', 'Net 1 val', 'Net 2 train', 'Net 2 val', 'Net 3 train', 'Net 3 val', 'Net 4 train', 'Net 4 val', 'Location', 'Northeast', 'fontsize',11);
+
+title('Training and validation errors per epoch', 'fontsize',14)
+xlabel('Epoch', 'fontsize', 12)
+ylabel('Classification error', 'fontsize',12)
 
 %% Definition of functions
 function [weights, thresholds] = initialize(M, input_size, output_size)
@@ -216,6 +281,8 @@ g = sigmoid(bval) .* (1 - sigmoid(bval));
 end
 
 function C = classification_error(target, outputs)
-length_valset = size(target, 2);    
+length_valset = size(target, 2);
+outputs = double(bsxfun(@eq, outputs, max(outputs, [], 1)));
+
 C = 1 / (2*length_valset) * sum(abs(outputs - target),'all');
 end
